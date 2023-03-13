24187a51b25a4b271e9f796b08d04eaa5595814b
Ahmad Subadri <ahmadsubadri953@gmail.com>
2023-03-13 12:23:00 +0700

    menu master data and student

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Master Data
+$route['dashboard/data-yayasan'] = 'Master_data/Yayasan_master';
