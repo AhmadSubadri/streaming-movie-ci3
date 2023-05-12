22f840b18e03cfba4e6990e530adf14315641e4e
Ahmad Subadri <ahmadsubadri953@gmail.com>
2023-05-12 14:08:47 +0700

    restrictions on the selection of course options in the distribution of courses

--- application/modules/Akademik/views/kurikulum_prodi/index.php
+++ application/modules/Akademik/views/kurikulum_prodi/index.php
@@ -81,29 +81,40 @@
         </div>
     </div>
 </div>
-<!-- Sertakan library Select2 -->
-<!-- ... Your HTML code ... -->
 
 <script>
+    var selectedOptions = {}; // Variabel global untuk menyimpan matakuliah yang dipilih di setiap field
+
     function persebaran_fields(id) {
         var kode_prodi = document.getElementById('kode_prodi').value;
         var existingForms = document.querySelectorAll('[id^="autokommatakuliah"]');
-        start = existingForms.length + 1;
+        var start = existingForms.length + 1;
         var objTo = document.getElementById('persebaran_fields' + id);
         var divtest = document.createElement('tr');
-        divtest.setAttribute('class', 'form-group removeclass' + start);
-        var rdiv = 'removeclass' + start;
+        divtest.setAttribute('class', 'form-group removeclass');
+        var rdiv = 'removeclass';
         divtest.innerHTML =
             '<td>' +
             '<select class="form-select custom-select" name="matakuliah[]" id="autokommatakuliah' + start + '"></select>' +
             '</td>' +
             '<td><input name="is_wajib[]" type="checkbox" id="is_wajib' + start + '" class="chk-col-red" /><label for="is_wajib' + start + '"></label></td>' +
             '<td><input name="is_paket[]" type="checkbox" id="is_paket' + start + '" class="chk-col-red" /><label for="is_paket' + start + '"></label></td>' +
-            '<td><button class="btn btn-danger btn-sm" type="button" onclick="remove_persebaran_fields(' + start + ');"><i class="fa fa-minus"></i></button></td>';
+            '<td><button class="btn btn-danger btn-sm" type="button" onclick="remove_persebaran_fields(this, \'' + start + '\');"><i class="fa fa-minus"></i></button></td>';
 
         objTo.appendChild(divtest);
         var selectElement = document.getElementById('autokommatakuliah' + start);
         selectElement.style.width = '100%';
+
+        // Mengambil matakuliah yang telah dipilih sebelumnya
+        var selectedMatakuliah = [];
+        for (var i = 0; i < existingForms.length; i++) {
+            var existingSelect = existingForms[i];
+            var selectedOption = existingSelect.options[existingSelect.selectedIndex];
+            if (selectedOption.value !== "") {
+                selectedMatakuliah.push(selectedOption.value);
+            }
+        }
+
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '<?= site_url("administrator/kurikulum-prodi/autocomplete"); ?>', true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -111,9 +122,13 @@
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 var data = JSON.parse(xhr.responseText);
                 var html = '';
-                html += '<option selected="selected" disabled="disabled">Select Mtakuliah</option>';
+                html += '<option selected="selected" disabled="disabled">Select Matakuliah</option>';
                 for (var i = 0; i < data.length; i++) {
-                    html += '<option value="' + data[i].kode_mk + '">' + data[i].nama_mk + '</option>';
+                    // Memeriksa apakah matakuliah telah dipilih sebelumnya
+                    if (!selectedMatakuliah.includes(data[i].kode_mk)) {
+                        html += '<option value="' + data[i].kode_mk + '">' + data[i].nama_mk + '</option>';
+                        selectedOptions[start] = data[i].kode_mk; // Simpan nilai yang dipilih
+                    }
                 }
                 selectElement.innerHTML = html;
                 $(selectElement).select2();
@@ -122,7 +137,194 @@
         xhr.send('id=' + kode_prodi);
     }
 
-    function remove_persebaran_fields(rid) {
-        $('.removeclass' + rid).remove();
+    function remove_persebaran_fields(button, fieldId) {
+        var row = button.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+
+        // Update selected options for remaining fields
+        var remainingFields = document.querySelectorAll('[id^="autokommatakuliah"]');
+        for (var i = 0; i < remainingFields.length; i++) {
+            var remainingField = remainingFields[i];
+            var remainingFieldId = remainingField.id.replace('autokommatakuliah', ''); // Dapatkan nomor field dari ID
+            if (remainingFieldId !== fieldId) {
+                // Hanya perbarui pilihan jika field belum dihapus
+                var selectedOption = remainingField.options[remainingField.selectedIndex];
+                if (selectedOption && selectedOption.value === selectedOptions[fieldId]) {
+                    remainingField.selectedIndex = 0;
+                    delete selectedOptions[fieldId]; // Hapus nilai yang dihapus
+                }
+            }
+        }
     }
+
+    // Fungsi untuk menghapus semua field
+    function removeAllFields() {
+        var fields = document.querySelectorAll('[id^="autokommatakuliah"]');
+        for (var i = 0; i < fields.length; i++) {
+            var field = fields[i];
+            var fieldId = field.id.replace('autokommatakuliah', ''); // Dapatkan nomor field dari ID
+            field.parentNode.parentNode.remove(); // Hapus baris dari tabel
+            delete selectedOptions[fieldId]; // Hapus nilai dari selectedOptions
+        }
+    }
+
+    // function persebaran_fields(id) {
+    //     var kode_prodi = document.getElementById('kode_prodi').value;
+    //     var existingForms = document.querySelectorAll('[id^="autokommatakuliah"]');
+    //     var start = existingForms.length + 1;
+    //     var objTo = document.getElementById('persebaran_fields' + id);
+    //     var divtest = document.createElement('tr');
+    //     divtest.setAttribute('class', 'form-group removeclass');
+    //     var rdiv = 'removeclass';
+    //     divtest.innerHTML =
+    //         '<td>' +
+    //         '<select class="form-select custom-select" name="matakuliah[]" id="autokommatakuliah' + start + '"></select>' +
+    //         '</td>' +
+    //         '<td><input name="is_wajib[]" type="checkbox" id="is_wajib' + start + '" class="chk-col-red" /><label for="is_wajib' + start + '"></label></td>' +
+    //         '<td><input name="is_paket[]" type="checkbox" id="is_paket' + start + '" class="chk-col-red" /><label for="is_paket' + start + '"></label></td>' +
+    //         '<td><button class="btn btn-danger btn-sm" type="button" onclick="remove_persebaran_fields(this, \'' + start + '\');"><i class="fa fa-minus"></i></button></td>';
+
+    //     objTo.appendChild(divtest);
+    //     var selectElement = document.getElementById('autokommatakuliah' + start);
+    //     selectElement.style.width = '100%';
+
+    //     // Mengambil matakuliah yang telah dipilih sebelumnya
+    //     var selectedMatakuliah = [];
+    //     for (var i = 0; i < existingForms.length; i++) {
+    //         var existingSelect = existingForms[i];
+    //         var selectedOption = existingSelect.options[existingSelect.selectedIndex];
+    //         if (selectedOption.value !== "") {
+    //             selectedMatakuliah.push(selectedOption.value);
+    //         }
+    //     }
+
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.open('POST', '<?= site_url("administrator/kurikulum-prodi/autocomplete"); ?>', true);
+    //     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //             var data = JSON.parse(xhr.responseText);
+    //             var html = '';
+    //             html += '<option selected="selected" disabled="disabled">Select Matakuliah</option>';
+    //             for (var i = 0; i < data.length; i++) {
+    //                 // Memeriksa apakah matakuliah telah dipilih sebelumnya
+    //                 if (!selectedMatakuliah.includes(data[i].kode_mk)) {
+    //                     html += '<option value="' + data[i].kode_mk + '">' + data[i].nama_mk + '</option>';
+    //                 }
+    //             }
+    //             selectElement.innerHTML = html;
+    //             $(selectElement).select2();
+    //         }
+    //     };
+    //     xhr.send('id=' + kode_prodi);
+    // }
+
+    // function remove_persebaran_fields(button, selectedValue) {
+    //     var row = button.parentNode.parentNode;
+    //     row.parentNode.removeChild(row);
+
+    //     // Update selected options for remaining fields
+    //     var remainingFields = document.querySelectorAll('[id^="autokommatakuliah"]');
+    //     for (var i = 0; i < remainingFields.length; i++) {
+    //         var remainingField = remainingFields[i];
+    //         if (remainingField.value === selectedValue) {
+    //             remainingField.selectedIndex = 0;
+    //         }
+    //     }
+    // }
+
+
+
+
+    // function persebaran_fields(id) {
+    //     var kode_prodi = document.getElementById('kode_prodi').value;
+    //     var existingForms = document.querySelectorAll('[id^="autokommatakuliah"]');
+    //     start = existingForms.length + 1;
+    //     var objTo = document.getElementById('persebaran_fields' + id);
+    //     var divtest = document.createElement('tr');
+    //     divtest.setAttribute('class', 'form-group removeclass' + start);
+    //     var rdiv = 'removeclass' + start;
+    //     divtest.innerHTML =
+    //         '<td>' +
+    //         '<select class="form-select custom-select" name="matakuliah[]" id="autokommatakuliah' + start + '"></select>' +
+    //         '</td>' +
+    //         '<td><input name="is_wajib[]" type="checkbox" id="is_wajib' + start + '" class="chk-col-red" /><label for="is_wajib' + start + '"></label></td>' +
+    //         '<td><input name="is_paket[]" type="checkbox" id="is_paket' + start + '" class="chk-col-red" /><label for="is_paket' + start + '"></label></td>' +
+    //         '<td><button class="btn btn-danger btn-sm" type="button" onclick="remove_persebaran_fields(' + start + ');"><i class="fa fa-minus"></i></button></td>';
+
+    //     objTo.appendChild(divtest);
+    //     var selectElement = document.getElementById('autokommatakuliah' + start);
+    //     selectElement.style.width = '100%';
+
+    //     // Mengambil matakuliah yang telah dipilih sebelumnya
+    //     var selectedMatakuliah = [];
+    //     for (var i = 0; i < existingForms.length; i++) {
+    //         var existingSelect = existingForms[i];
+    //         var selectedOption = existingSelect.options[existingSelect.selectedIndex];
+    //         if (selectedOption.value !== "") {
+    //             selectedMatakuliah.push(selectedOption.value);
+    //         }
+    //     }
+
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.open('POST', '<?= site_url("administrator/kurikulum-prodi/autocomplete"); ?>', true);
+    //     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //             var data = JSON.parse(xhr.responseText);
+    //             var html = '';
+    //             html += '<option selected="selected" disabled="disabled">Select Matakuliah</option>';
+    //             for (var i = 0; i < data.length; i++) {
+    //                 // Memeriksa apakah matakuliah telah dipilih sebelumnya
+    //                 if (!selectedMatakuliah.includes(data[i].kode_mk)) {
+    //                     html += '<option value="' + data[i].kode_mk + '">' + data[i].nama_mk + '</option>';
+    //                 }
+    //             }
+    //             selectElement.innerHTML = html;
+    //             $(selectElement).select2();
+    //         }
+    //     };
+    //     xhr.send('id=' + kode_prodi);
+    // }
+
+    // function persebaran_fields(id) {
+    //     var kode_prodi = document.getElementById('kode_prodi').value;
+    //     var existingForms = document.querySelectorAll('[id^="autokommatakuliah"]');
+    //     start = existingForms.length + 1;
+    //     var objTo = document.getElementById('persebaran_fields' + id);
+    //     var divtest = document.createElement('tr');
+    //     divtest.setAttribute('class', 'form-group removeclass' + start);
+    //     var rdiv = 'removeclass' + start;
+    //     divtest.innerHTML =
+    //         '<td>' +
+    //         '<select class="form-select custom-select" name="matakuliah[]" id="autokommatakuliah' + start + '"></select>' +
+    //         '</td>' +
+    //         '<td><input name="is_wajib[]" type="checkbox" id="is_wajib' + start + '" class="chk-col-red" /><label for="is_wajib' + start + '"></label></td>' +
+    //         '<td><input name="is_paket[]" type="checkbox" id="is_paket' + start + '" class="chk-col-red" /><label for="is_paket' + start + '"></label></td>' +
+    //         '<td><button class="btn btn-danger btn-sm" type="button" onclick="remove_persebaran_fields(' + start + ');"><i class="fa fa-minus"></i></button></td>';
+
+    //     objTo.appendChild(divtest);
+    //     var selectElement = document.getElementById('autokommatakuliah' + start);
+    //     selectElement.style.width = '100%';
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.open('POST', '<?= site_url("administrator/kurikulum-prodi/autocomplete"); ?>', true);
+    //     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //             var data = JSON.parse(xhr.responseText);
+    //             var html = '';
+    //             html += '<option selected="selected" disabled="disabled">Select Mtakuliah</option>';
+    //             for (var i = 0; i < data.length; i++) {
+    //                 html += '<option value="' + data[i].kode_mk + '">' + data[i].nama_mk + '</option>';
+    //             }
+    //             selectElement.innerHTML = html;
+    //             $(selectElement).select2();
+    //         }
+    //     };
+    //     xhr.send('id=' + kode_prodi);
+    // }
+
+    // function remove_persebaran_fields(rid) {
+    //     $('.removeclass' + rid).remove();
+    // }
 </script>
